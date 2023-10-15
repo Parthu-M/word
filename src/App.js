b9694ec4cd743cb90838286ceaab805db461babb
parthu <parthumajjiga2004@gmail.com>
2023-10-15 15:44:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import {WordCount} from './components/Page.js';
+function App() {
+  return (
+    <div className="App">
+      <WordCount/>
+    </div>
+  );
+}
+
+export default App;
